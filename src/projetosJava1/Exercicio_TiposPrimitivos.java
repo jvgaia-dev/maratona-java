3a38d48c86d2478fdf0e9c93d4af1f529a68437d
Jvgaia <jvgaia30@gmail.com>
2025-03-31 10:44:44 -0300

    Arquivos iniciais de estudos

---
+++ src/projetosJava1/Exercicio_TiposPrimitivos.java
@@ -0,0 +1,13 @@
+package projetosJava1;
+
+public class Exercicio_TiposPrimitivos {
+    public static void main(String[] args) {
+        String nome = "João";
+        String endereco = "Fenda do Bikini";
+        double salario = 1400.25;
+        String frase = ("Eu " + nome + ", morando no endreço " + endereco + " confirmo que recebi o salário de " + salario + " na data 10/03/2025");
+
+        System.out.println(frase);
+    }
+
+}
